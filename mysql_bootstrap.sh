23bcef23a2318328c8cd95771be2e33648ddc60a
Synchro <marcus@synchromedia.co.uk>
2014-08-30 00:19:00 +0200

    Another wait approach

--- mysql_bootstrap.sh
+++ mysql_bootstrap.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 mysqld_safe &
 # Wait until MySQL is up
-wait $!
+while ! mysqladmin ping; do
+    sleep 1
+done
 mysql -e 'GRANT ALL PRIVILEGES ON *.* TO \"root\"@\"%\" WITH GRANT OPTION;'
