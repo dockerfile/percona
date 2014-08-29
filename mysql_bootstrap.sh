175ea042edf72e9156a87a278fc2e13e3c91686e
Synchro <marcus@synchromedia.co.uk>
2014-08-30 00:41:24 +0200

    Fix mysql check

--- mysql_bootstrap.sh
+++ mysql_bootstrap.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 mysqld_safe &
 # Wait until MySQL is up
-while ! mysqladmin ping; do
-    sleep 1
-done
-mysql -e 'GRANT ALL PRIVILEGES ON *.* TO \"root\"@\"%\" WITH GRANT OPTION;'
+mysqladmin --silent --wait=20 ping || exit 1
+mysql -e 'GRANT ALL PRIVILEGES ON *.* TO "root"@"%" WITH GRANT OPTION;'
